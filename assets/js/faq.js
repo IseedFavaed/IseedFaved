4a84494c95f7e6b6bb3153716512259bbac35356
Felipe <felipe.iseedfaved@gmail.com>
2022-04-06 15:18:18 -0300

    Add msg de Cookies no site

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -2,3 +2,6 @@ function mostrar(classe){
     document.getElementsByClassName('li-p'+classe)[0].style.display = "block";
 }
 
+function esconder(){
+    document.getElementById('msgCookies').style.display = "none";
+}
